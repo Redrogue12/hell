66bec13779c6f69ff51269a2df3d6fcddd1aa331
Redrogue12 <exgc12@gmail.com>
2020-04-12 19:58:44 -0400

    upload build folder

---
+++ build/precache-manifest.d1d6989743df662f9ebbf318cfe101cf.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebd80d892bdf2109ba8aa89c9725ab0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bfa65d272931d9d4cea",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b713a94b9255a59ce8cb",
+    "url": "/static/css/main.5b082aa6.chunk.css"
+  },
+  {
+    "revision": "4bfa65d272931d9d4cea",
+    "url": "/static/js/2.ef242666.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.ef242666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b713a94b9255a59ce8cb",
+    "url": "/static/js/main.c21a4f32.chunk.js"
+  },
+  {
+    "revision": "cff2ddadaae390e40677",
+    "url": "/static/js/runtime-main.9026c62b.js"
+  },
+  {
+    "revision": "0c16f7a45c9a98d45fc6de98d3a966a8",
+    "url": "/static/media/adnan.0c16f7a4.jpeg"
+  },
+  {
+    "revision": "7e02423a0cf9fe8c67c2d7727295036a",
+    "url": "/static/media/alberto.7e02423a.jpeg"
+  },
+  {
+    "revision": "033410edea373aee32186a7e0b3aea67",
+    "url": "/static/media/cesar.033410ed.jpeg"
+  },
+  {
+    "revision": "55a831b89cdbc7f0a05a446b909906f0",
+    "url": "/static/media/christian.55a831b8.jpeg"
+  },
+  {
+    "revision": "ef383332b685420465f0268548b602ec",
+    "url": "/static/media/chucho.ef383332.jpeg"
+  },
+  {
+    "revision": "901d108e698ec97ab40db81f8b7fec15",
+    "url": "/static/media/edgar.901d108e.jpeg"
+  },
+  {
+    "revision": "9d9412d30dff7f4bf9bc467605e8d72f",
+    "url": "/static/media/gaby.9d9412d3.jpeg"
+  },
+  {
+    "revision": "27047ee3bc087ca5b7ab8b480619ccd6",
+    "url": "/static/media/jediel.27047ee3.jpeg"
+  },
+  {
+    "revision": "17b9d6eb2ef8f62eb80fa8de6dcf501e",
+    "url": "/static/media/josean.17b9d6eb.jpeg"
+  },
+  {
+    "revision": "dd5688427e14304914d327e7c552073b",
+    "url": "/static/media/juan.dd568842.jpeg"
+  },
+  {
+    "revision": "a645af9927fabb6360b814bc9ef4636c",
+    "url": "/static/media/rada.a645af99.jpeg"
+  },
+  {
+    "revision": "7692e6afee515cc432393cb9544d3e87",
+    "url": "/static/media/steve.7692e6af.jpeg"
+  },
+  {
+    "revision": "3151ee21e585356a4b7040fa2a70bf1f",
+    "url": "/static/media/tiffany.3151ee21.jpeg"
+  }
+]);
